dfa51566e56913128c9cb44e3650a468d8682c32
jamiro <jamirograntsaan@gmail.com>
2020-08-26 00:19:04 +0200

    updated template to be representative of modules

--- modules/template.py
+++ modules/template.py
@@ -1,14 +1,19 @@
+from components.logger import Logger
+
 class Template:
     def __init__(self):
         self.dependencies = []
         self.capabilities = ["timed", "async"]
         self.timing = {"unit": "seconds", "count":2}
+        # do not add init stuff
 
     def doStuff(self):
-        print("Doing stuff...")
+        self.logger("Doing stuff...")
         
 
     def startrun(self):
         """this is what gets called by main"""
+        # init stuff..
+        self.logger = Logger("TEMPLATE").logger
         self.doStuff()
         
