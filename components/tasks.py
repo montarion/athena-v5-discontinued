1e739651348b206322c56d50a94d8b3b38121453
jamiro <jamirograntsaan@gmail.com>
2020-08-26 02:10:15 +0200

    now allows for argument insertion via dictionary

--- components/tasks.py
+++ components/tasks.py
@@ -10,12 +10,11 @@ class Tasks:
     def __init__(self):
         pass
 
-    def createtask(self, functarget, time, unit, tag="user task"):
-        task = getattr(schedule.every(time), unit).do(functarget).tag(tag)
-        #if unit == "seconds":
-        #    schedule.every(time).seconds.do(functarget)
-        #if unit == "minutes":
-        #    schedule.every(time).minutes.do(functarget)
+    def createtask(self, functarget, time, unit, argdict=None, tag="user task"):
+        if not argdict:
+            task = getattr(schedule.every(time), unit).do(functarget).tag(tag)
+        else:
+            task = getattr(schedule.every(time), unit).do(functarget, **argdict).tag(tag)
         return task
 
     def removetask(self, tag):
@@ -23,3 +22,6 @@ class Tasks:
 
     def runall(self):
         schedule.run_pending()
+
+    def runfirst(self):
+        schedule.run_all()
