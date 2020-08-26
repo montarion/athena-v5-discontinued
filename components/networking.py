589e5da1295bb3e2b70b0c956cefab95246118ef
jamiro <jamirograntsaan@gmail.com>
2020-08-26 02:09:19 +0200

    WIP trying to implement anime with network functionality

--- components/networking.py
+++ components/networking.py
@@ -1,31 +1,53 @@
 import websockets, asyncio, json
-from components.database import Database
+from components.logger import Logger
 
 class Networking:
 
-    def __init__(self):
-        self.memdatabase = Database("memory")
-        self.database = Database()
-
+    def __init__(self, database=None):
+        self.database = database
         self.loop = asyncio.new_event_loop()
+        self.logger = Logger("Networking").logger
 
     async def runserver(self, websocket, path):
-        data = await websocket.recv()
-        msg = json.loads(data)
-        print(msg)
-        await self.msghandler(websocket, msg)
+        while True:
+            data = await websocket.recv()
+            msg = json.loads(data)
+            print(msg)
+            await self.msghandler(websocket, msg)
 
     def createid(self):
-        curid = len(self.database.gettable("users"))
-        newid = curid + 1
-        return int(newid)
+        table = self.database.gettable("users")
+        self.logger(table, "debug", "red")
+        if table["status"] == 200:
+            curid = len(table)
+            newid = curid + 1
+            return int(newid)
+        else:
+            return table
 
     def findtarget(self, query):
-        pass
+        idlist = []
+        res = self.database.gettable("users")
+        for key in res["resource"]:
+            dic = res["resource"]
+            #searchres = dic[key].get(query, False)
+            searchres = query in dic[key].values()
+            if searchres:
+                idlist.append(dic[key]["id"])
+        self.logger(idlist, "debug", "green")
+        return idlist
 
-    async def send(self, message, targetid):
-        socket = self.memdatabase.query("id", targetid, "networking")["socket"]
-        await socket.send(message)
+    async def send(self, message, targetidlist):
+        returnmsg = {"success": [], "failure": []}
+        for targetid in targetidlist:
+            #socket = self.database.query(targetid, "networking")["socket"]
+            socket = self.database.membase[targetid]["socket"]
+            if type(message) == dict:
+                message = json.dumps(message)
+            await socket.send(message)
+            returnmsg["success"].append(targetid)
+
+        return returnmsg
 
     async def msghandler(self, websocket, msg):
         category = msg["category"]
@@ -44,14 +66,31 @@ class Networking:
                 else:
                     #create new id
                     id = self.createid()
-                self.memdatabase.update({"id": id, "socket": websocket}, "id", id, "networking")
-                self.database.update({"id": id, "name": name}, "id", id, "users")
+                self.database.membase[id] = {"socket": websocket}
+                self.database.write(id, {"id":id, "name": name}, "users")
                 returnmsg = json.dumps({"category":"admin", "type":"signinresponse", "data":{"id":id}})
-                await self.send(returnmsg, id)
+                await self.send(returnmsg, [id])
+
+    def messagebuilder(self, category, msgtype, data={}, metadata={}, target="all"):
+        msg = json.dumps({"category":category, "type":msgtype, "data":data, "metadata":metadata})
+        if target == "all":
+            idlist = list(self.database.membase.keys())
+        if type(target) == list:
+            idlist = target
+        elif type(target) == int:
+            idlist = [target]
+
+        result = {"success": [], "failure": []}
+
+        for id in idlist:
+            result = self.send(msg, idlist)
+        return result
 
     def startserving(self):
+        self.logger(self.database.query("favfruit", "test"), "debug", "yellow")
+
         serveserver = websockets.server.serve(self.runserver, "0.0.0.0", 8765, loop=self.loop)
         asyncio.set_event_loop(self.loop)
         self.loop.run_until_complete(serveserver)
-        print("waiting...")
+        self.logger("waiting...")
         self.loop.run_forever()
---
+++ modules/anime.py
@@ -0,0 +1,154 @@
+import os, requests, json, feedparser, re, time
+
+from components.database import Database
+from components.logger import Logger
+
+class Anime:
+    def __init__(self, Networking=None):
+        self.dependencies = ["Networking"]
+        self.capabilities = ["timed"]
+        self.timing = {"unit": "minutes", "count":2}
+        self.networking = Networking
+ 
+        # other init stuff happens in startrun
+
+    def getshows(self, number = 1):
+        base = f"https://nyaa.si/?page=rss&q={self.publishchoice}+%2B+[1080p]&c=1_2&f=2"
+        feed = feedparser.parse(base)
+        entries = feed.entries
+        sessiondict = {}
+        x = 0
+        while x < number:
+            try:
+                entry = entries[x]
+            except IndexError:
+                self.logger(f"Tried index {x} and failed.")
+                break
+
+            title, show, episode = self.cleantitle(entry["title"])
+            link = entry["link"]
+            if show in self.watchlist:
+                sessiondict["command"] = "anime"
+                sessiondict["title"] = show
+                sessiondict["episode"] = episode
+                if show not in self.maindict:
+                    imagelink, bannerlink, maxepisodes = self.getinfo(show) #TODO add synopsis
+                    self.maindict[show] = {"art":{}, "meta":{}}
+                    self.maindict[show]["art"]["cover"] = imagelink
+                    self.maindict[show]["art"]["banner"] = bannerlink
+                    self.maindict[show]["meta"]["maxepisodes"] = maxepisodes
+                sessiondict["art"] = {}
+                sessiondict["art"]["cover"] = self.maindict[show]["art"]["cover"]
+                sessiondict["art"]["banner"] = self.maindict[show]["art"]["banner"]
+
+                self.maindict[show]["lastep"] = episode
+                animedict = self.dbobj.gettable("anime")["resource"]
+                self.logger(f"current: {show}")
+                if animedict.get("lastshow", {"title":"show"})["title"] != "pizza": #show:
+                    self.download(show, link)
+                    ct = int(time.time())
+                    self.maindict[show]["aired_at"] = ct
+                    sessiondict["aired_at"] = ct
+                    #targetlist = settings().checkavailability("computing")["resource"]
+                    #if len(targetlist) == 0:
+                    #Event().anime("aired")
+                    idlist = self.networking.findtarget("greytest")
+                    category = "anime"
+                    type = "latest"
+                    artdict = {"cover": imagelink, "banner": bannerlink}
+                    data = {"title":show, "lastep": episode, "art":artdict, "aired_at":ct}
+                    metadata = {"status": 200}
+                    self.logger(f"Starting to send message..")
+                    res = self.networking.messagebuilder(category, type, data, metadata, "all")
+                    self.logger(res)
+                    Database().write("lastshow", sessiondict, "anime")
+                x += 1
+            else:
+                number += 1
+                x += 1
+        Database().write("animedict", self.maindict, "anime")
+
+
+
+
+    def cleantitle(self, title):
+        pattern = "\[HorribleSubs\] (.*) - ([0-9]*) \[1080p\].mkv"
+        res = re.search(pattern, title)
+        show = res.group(1)
+        episode = res.group(2)
+        newtitle = f"{show} - {episode}.mkv"
+        return newtitle, show, episode
+
+    def getinfo(self, name):
+        query = "query($title: String){Media (search: $title, type: ANIME){episodes, bannerImage, coverImage{extraLarge}}}" # this is graphQL, not REST
+        variables = {'title': name}
+        url = 'https://graphql.anilist.co'
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        preurl = json.loads(response.text)["data"]["Media"]
+
+        coverdict = preurl["coverImage"]
+        bannerurl = preurl["bannerImage"]
+        maxepisodes = preurl["episodes"]
+
+        coverurl = coverdict[list(coverdict.keys())[0]]
+        if bannerurl == None:
+            bannerurl = coverurl
+
+        return coverurl, bannerurl, maxepisodes
+
+    def download(self, show, link):
+        # check if folder exists:
+        root = "/mnt/raspidisk/files/anime/"
+        truepath = os.path.join(root, "\ ".join(show.split(" ")))
+        check = os.path.isdir(truepath)
+        if not check:
+            self.logger("creating folder for {}".format(show), "debug")
+            precommand = "sudo -H -u pi bash -c \""
+            command = "mkdir {}".format(truepath)
+            postcommand = "\""
+            os.system(precommand + command + postcommand)
+        else:
+            self.logger("folder {} already existed".format(show), "debug")
+        precommand = "sudo -H -u pi bash -c \""
+        command = (precommand + r"deluge-console 'add -p {} {}'".format(truepath, link) + "\"")
+        os.system(command)
+
+    def findshows(self):
+        from bs4 import BeautifulSoup
+
+        url = "https://horriblesubs.info/current-season/"
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, "html.parser")
+        preshows = soup.find_all(class_="ind-show")
+        showlist = []
+        chosenlist = []
+        for show in preshows:
+            showlist.append(show.find("a")["title"])
+
+        for i, show in enumerate(showlist):
+            print("Show number {} is: {}".format(i + 1, show))
+            interest = input("w to add {}".format(show))
+            if interest == "w":
+                chosenlist.append(show)
+        result = self.dbobj.write("watchlist", chosenlist, "anime")
+
+        return result
+
+    def startrun(self):
+        self.logger = Logger("Anime").logger
+        self.dbobj = Database()
+        self.logger(self.networking, "debug", "yellow")
+        prelist = self.dbobj.query("watchlist", "anime")
+        if prelist["status"] == 200:
+            self.watchlist = prelist["resource"]
+        else:
+            self.watchlist = self.findshows()
+
+        self.publishchoice = "HorribleSubs"
+        predict = self.dbobj.query("maindict", "anime")
+        if predict["status"] == 200:
+            self.maindict = predict["resource"]
+        else:
+            self.maindict = {}
+
+        self.getshows()
