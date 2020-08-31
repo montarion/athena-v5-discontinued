9ee374d9c2fbc83e9662e098ba4e0808700e7824
jamiro <jamirograntsaan@gmail.com>
2020-08-31 03:01:57 +0200

    implemented intermodule communication through watcher.py and redis

---
+++ components/watcher.py
@@ -0,0 +1,83 @@
+import redis, json, threading
+from components.logger import Logger
+from time import sleep
+
+class Watcher:
+    def __init__(self):
+        self.logger = Logger("Watcher").logger
+        self.r = redis.Redis(host='localhost', port=6379, db=0)
+        self.p = self.r.pubsub(ignore_subscribe_messages=True)
+
+        self.listenstop = False
+        self.subscriptions = []
+        self.funcdict = {}
+        self.triggerdict = {}
+
+    def startlisten(self):
+        for x in self.subscriptions:
+            self.p.subscribe(x)
+        self.listenstop = False # allow execution
+        if len(self.subscriptions) > 0:
+            threading.Thread(target=self.listen).start()
+            self.logger("started")
+        else:
+            self.logger("no subscriptions yet")
+
+    def listen(self):
+        while True:
+            msg = self.p.get_message()
+            if msg:
+                channel = msg["channel"].decode()
+                data = msg["data"].decode()
+                try:
+                    data = json.loads(data)
+                except Exception as e:
+                    self.logger(e)
+                self.logger(f"Got message for: {channel}")
+                if channel in self.funcdict.keys():
+                    channeldict = self.funcdict[channel]
+                    trigger = channeldict["trigger"]["returnvalue"]
+                    self.logger(trigger)
+                    self.logger(data.keys())
+                    if trigger in data.keys():
+
+                        returnvalue = data[trigger]
+                        # execute function
+                        exec_class = channeldict["result"]["class"]
+                        exec_func = channeldict["result"]["function"]
+                        args = channeldict["result"]["args"]
+                        self.logger(f"full args: {args}")
+                        for key, val in args.items():
+                            args[key] = data[val]
+                        sleep(2)
+                        t = getattr(exec_class, exec_func)(**args)
+                        self.logger(t)
+            if self.listenstop:
+                break
+        
+    def execute(self, classname, funcname):
+        # skip
+        pass
+
+    def register(self, regdata):
+        """
+            Registers function to be ran when a specific function is ran
+            Usage: regdata = {'trigger':{'class':'foo', 'returnvalue':'title'}, 
+                             'result':{'class':Bar(), 'function':'funcname', 'args':{'foo':'bar'}}
+
+            trigger[class] is name of the class you want updates from.
+            result[class] is class object(self in a class) that you want to call when triggered
+            arg keys must match the name of the parameter.
+            publish a message like this: r.publish("foo", json.dumps({"bar":"argument content"})
+        """
+        name = regdata["trigger"]["class"]
+        self.subscriptions.append(name)
+
+        tmpdict = regdata
+        self.funcdict[name] = tmpdict
+
+        self.triggerdict[name] = regdata["trigger"]
+        self.logger(f"subscribed to: {self.subscriptions}")
+        self.listenstop = True
+        self.logger("stopped")
+        self.startlisten()
